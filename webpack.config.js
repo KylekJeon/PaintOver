6e3e43099d7deba01d162e96fc3dcd384a84ed7c
Kyle Jeon <kylekjeon@gmail.com>
2017-01-05 08:48:51 -0500

    Add painter mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  entry: "./paintover.js",
+  entry: "./lib/paintover.js",
   output: {
-    filename: "./bundle.js",
+    filename: "./lib/bundle.js",
   },
   module: {
     loaders: [
